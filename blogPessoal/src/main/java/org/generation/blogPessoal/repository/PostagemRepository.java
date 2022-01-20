d3b882ff1d31a6024b24dfc702cbdf7f94ce992a
Rodrigo Baba <rodrigonuciatelli@gmail.com>
2022-01-19 22:31:55 -0300

    Inserindo Put, Post e Delete na Classe PostagemController

--- blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -6,8 +6,10 @@ import org.generation.blogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-	
+
 	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 
+	public List<Postagem> findAllByTextoContainingIgnoreCase(String texto);
 	
+	public Postagem findByTitulo(String titulo);
 }
