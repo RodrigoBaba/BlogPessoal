b910e978770fa39c1ec38a6f73ec24d97e042b19
Rodrigo Baba <rodrigonuciatelli@gmail.com>
2022-02-03 19:12:29 -0300

    @RodrigoBaba [Introduzindo Swagger e Deploy]

--- src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -16,27 +16,23 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);		
+		auth.userDetailsService(userDetailsService);
+		auth.inMemoryAuthentication().withUser("root").password(passwordEncoder().encode("root")).authorities("ADMIN");
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
-	protected void configure(HttpSecurity http) throws Exception { 
-		http.authorizeRequests()
-		.antMatchers("/usuarios/logar").permitAll()
-		.antMatchers("/usuarios/cadastrar").permitAll()
-		.anyRequest().authenticated()
-		.and().httpBasic()
-		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().cors()
-		.and().csrf().disable();
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll().antMatchers("/usuarios/cadastrar")
+				.permitAll().anyRequest().authenticated().and().httpBasic().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and().csrf().disable();
 	}
-	
+
 }
